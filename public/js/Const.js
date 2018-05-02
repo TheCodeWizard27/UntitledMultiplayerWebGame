859fd0eadbe716b2e9a6c07ebd8cef6215860fb4
TheCodeWizard27 <bschab@ws-bschab.bbc-be.local>
2018-05-02 16:21:22 +0200

    Direction konstanten hinzugefügt mit der createjs point klasse

--- public/js/Const.js
+++ public/js/Const.js
@@ -24,10 +24,18 @@ export const SERVER_CONFIGURATION = {
 		FPS: 30,
 		MAX_PLAYER: 4,
 		SPEED: 20,
+		MARKER_SPEED: 40,
 		SHOOT_COOLDOWN: 4,
 		GRID_WIDTH: 800,
 		GRID_HEIGHT: 600,
 		GRID_TILES_COUNT_X: 20,
 		GRID_TILES_COUNT_Y: 20
 	}
+};
+
+export const DIRECTION = {
+	UP: new createjs.Point(0,-1),
+	DOWN: new createjs.Point(0,1),
+	LEFT: new createjs.Point(-1,0),
+	RIGHT: new createjs.Point(1,0)
 };
