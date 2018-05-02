35cbe4f357c7d7cf401d400509ef1f9f06c496dc
TheCodeWizard27 <bschab@ws-bschab.bbc-be.local>
2018-05-02 15:40:04 +0200

    Player klasse kommentiert und strict mode hinzugefügt

--- public/js/Player.js
+++ public/js/Player.js
@@ -1,7 +1,17 @@
+'use strict';
+
+/**
+ * Player Class holds information about the position, the direction, the score, all markers.
+ * Each player has its own keyBuffer
+ */
 export default class Player{
-	constructor(graphics){
+	/**
+	 * Creates Player Object
+	 * @param graphics for deleting adding sprites to stage
+	 */
+	constructor(pos, graphics){
 		this._graphics = graphics;
-		this._pos = new createjs.Point(0,0);
+		this._pos = pos;
 		this._dir = new createjs.Point(0,1);
 		this._markers = [];
 		this._markCooldown = 0;
@@ -11,19 +21,33 @@ export default class Player{
 		this._sprite = this._graphics.createPlayer();
 	}
 	
+	/**
+	 * shoots a marker relative to pos and dir
+	 */
 	mark(){
 		if(this._markCooldown <= 0){
 			//TODO add Marker shoot code
 		}
 	}
 	
+	/**
+	 * adds input to player
+	 * @param key	keycode
+	 */
 	addKey(key){
 		this._keyBuffer.add(key);
 	}
+	/**
+	 * removes input to player
+	 * @param key	keycode
+	 */
 	removeKey(key){
 		this._keyBuffer.delete(key);
 	}
 	
+	/**
+	 * Deletes Player Sprite for deleting playerobj
+	 */
 	delete(){
 		this._graphics.removeSprite(this._sprite);
 	}
