c4ea043ba3dee7086b4371d9ab290d39ac9f5043
TheCodeWizard27 <bschab@ws-bschab.bbc-be.local>
2018-05-02 14:05:49 +0200

    Graphics Klasse mit singleton pattern hinzugefügt

--- public/js/Graphics.js
+++ public/js/Graphics.js
@@ -1,27 +1,68 @@
 "use strict";
 
-export class Graphics{
-	constructor(stage){
-		this._stage = stage;
-		this._playerSheet;
-		this._collectableSheet;
-		this._markerSheet;
-	}
+let Graphics = {
+	_graphics : null,
 	
-	createPlayer(){
-		//TODO create function with return of the player sprite
+	getInstance : function(stage){
+		if(this._graphics == null){
+			this._graphics = {
+				_stage : stage,
+				_playerSheet : null,
+				_collectableSheet : null,
+				_markerSheet : null,
+				_loader : new createjs.LoadQueue(false),
+				
+				/**
+				 * starts loading
+				 */
+				init : function(){
+					let manifest = [
+						{src: "pikachu.png", id: "pika"}
+					];
+					
+					this._loader.addEventListener("complete", this.completedLoading.bind(this));
+					this._loader.loadManifest(manifest, true, "img/");
+				},
+
+				/**
+				 * gets called after loading and initializes sprites
+				 */
+				completedLoading : function(){
+					//player sprite sheet
+					this._playerSheet = new createjs.SpriteSheet({
+						"images": [this._loader.getResult("pika")],
+						"frames": {"width": 64, "height": 64}
+					});
+				},
+				
+				/**
+				 * Creates Playersprite and adds it to the Stage for it to get drawn
+				 * @returns { Sprite }
+				 */
+				createPlayer : function(){
+					let tempSprite = new createjs.Sprite(this._playerSheet);
+					this._stage.addChild(tempSprite);
+					return tempSprite;
+				},
+				
+				createCollectable : function(){
+					//TODO createCollectable Sprite with return
+				},
+				createMarker : function(){
+					//TODO create marker sprite with return
+				},
+				
+				/**
+				 * Removes passed sprite on stage for deletion
+				 * @param sprite
+				 */
+				removeSprite : function(sprite){
+					this._stage.removeChild(sprite);
+				}
+			}
+		}
+		return this._graphics;
 	}
-	
-	createCollectable(){
-		//TODO create function with return of the Collectable Sprite
-	}
-	
-	createMarker(){
-		//TODO create function with return of the marker Sprite
-	}
-	
-	removeSprite(sprite){
-		this._stage.removeChild(sprite);
-	}
-	
-}
+};
+
+export default Graphics;
