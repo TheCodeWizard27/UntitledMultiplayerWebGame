7d0271e582c802fc42066cb51b55b774233f7e46
TheCodeWizard27 <bschab@ws-bschab.bbc-be.local>
2018-05-03 10:29:51 +0200

    GameObj Klasse mit addPlayer erweitert

--- public/js/GameObj.js
+++ public/js/GameObj.js
@@ -1,14 +1,28 @@
 'use strict';
 
+import Graphics from "./Graphics.js";
+import Player from "./Player.js";
+
+/**
+ * GameObj Object stores important information
+ * @type {{_gameObj : null, getInstance : (function() : null)}}
+ */
 let GameObj = {
-	_gameObj: null,			//"static" gameObj variable for singleton
+	_gameObj: null,
 	
+	/**
+	 * creates one time only GameObj instance
+	 */
 	getInstance: function() {
 		if(this._gameObj == null) {
 			//creates new GameObj Object
 			this._gameObj = {
 				_playerList: new Map(),
 				_collectables: [],
+				
+				addPlayer : function(identifier){
+					this._playerList[identifier] = new Player(new createjs.Point(0,0));
+				}
 			}
 		}
 		return this._gameObj;
