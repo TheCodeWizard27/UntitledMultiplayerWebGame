05858cec4833980bb6bbc952d9c31854a995c42c
TheCodeWizard27 <bschab@ws-bschab.bbc-be.local>
2018-05-03 10:30:22 +0200

    Sample animation hinzugefügt mit Singelton Objekt benutztung

--- public/js/Graphics.js
+++ public/js/Graphics.js
@@ -1,8 +1,14 @@
 "use strict";
 
+import GameController from "./GameController.js";
+
 let Graphics = {
 	_graphics : null,
 	
+	/**
+	 * singleton getInstance of Graphics function
+	 * @param stage	for adding removing sprites
+	 */
 	getInstance : function(stage){
 		if(this._graphics == null){
 			this._graphics = {
@@ -17,22 +23,37 @@ let Graphics = {
 				 */
 				init : function(){
 					let manifest = [
-						{src: "pikachu.png", id: "pika"}
+						{src: "link.png", id: "Link"}
 					];
-					
 					this._loader.addEventListener("complete", this.completedLoading.bind(this));
-					this._loader.loadManifest(manifest, true, "img/");
+					this._loader.loadManifest(manifest, true, "assets/textures/");
 				},
 
 				/**
 				 * gets called after loading and initializes sprites
 				 */
 				completedLoading : function(){
+					let gameController = GameController.getInstance();
+					
 					//player sprite sheet
 					this._playerSheet = new createjs.SpriteSheet({
-						"images": [this._loader.getResult("pika")],
-						"frames": {"width": 64, "height": 64}
+						framerate : 6,
+						"images": [this._loader.getResult("Link")],
+						"frames": {"width": 32, "height": 48},
+						"animations" : {
+							"fDown" : [0,0,"fDown",0.1],	//TODO Idle animations
+							"fLeft" : [4,4,"fLeft",0.1],
+							"fRight" : [8,8,"fRight",0.1],
+							"fUp" : [12,12,"fUp",0.1],
+							"wDown" : {frames : [1,2,3,0], next: "wDown", speed: 1},
+							"wLeft" : {frames : [5,6,7,4], next: "wLeft", speed: 1},
+							"wRight" : {frames : [9,10,11,8], next: "wRight", speed: 1},
+							"wUp" : {frames : [13,14,15,12], next: "wUp", speed: 1}
+						}
 					});
+					
+					//init game loop after loading
+					createjs.Ticker.addEventListener("tick", gameController.update.bind(gameController));
 				},
 				
 				/**
